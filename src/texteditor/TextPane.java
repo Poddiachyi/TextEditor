fc8a962f38308380bbe100e425c6c4c27636a4d5
AlexP <poddiachyi@gmail.com>
2016-05-07 21:48:17 +0300

    No patterns, images not saved to file

---
+++ src/texteditor/TextPane.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package texteditor;
+
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JTextPane;
+
+/**
+ *
+ * @author alexp
+ */
+public class TextPane extends JTextPane {
+
+    public TextPane() {
+        super();
+    }
+
+
+}
